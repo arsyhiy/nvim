29205c38f6e8a71ea7728f5099bd8f8e5fdaa865
arsyhiy <arsyhiy32@gmail.com>
2025-05-15 11:17:50 +0300

    change colorscheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,12 +1,12 @@
 return {
-  { "catppuccin/nvim" },
+  -- add dracula
+  { "Mofiqul/dracula.nvim" },
 
-   
+  -- Configure LazyVim to load dracula
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "catppuccin",
-
+      colorscheme = "dracula",
     },
   },
 }
