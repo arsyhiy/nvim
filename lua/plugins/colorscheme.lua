55214a478ddf088233bd16ca45584ef31f7670f9
arsen <arsyhiy32@gmail.com>
2025-06-08 16:43:30 +0300

    change colorscheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,12 +1,12 @@
 return {
   -- add dracula
-  { "Mofiqul/dracula.nvim" },
+  { "ellisonleao/gruvbox.nvim" },
 
   -- Configure LazyVim to load dracula
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "dracula",
+      colorscheme = "gruvbox",
     },
   },
 }
