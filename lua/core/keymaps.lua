7137a4d1f813ff679e5ed28f4af1619f95cab448
arsyhiy <arsyhiy32@gmail.com>
2026-04-23 20:09:28 +0300

    update keymap

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -6,4 +6,4 @@ keymap.set("n", "<leader>w", ":w<CR>")
 
 keymap.set("n", "<leader>q", ":q<CR>")
 
-vim.keymap.set("n", "<leader>e", ":Neotree toggle<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>e", ":Neotree filesystem reveal toggle<CR>", { noremap = true, silent = true })
