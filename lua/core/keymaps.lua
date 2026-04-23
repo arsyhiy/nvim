6d560993c253c6f363809bd200ffa92d12097248
arsyhiy <arsyhiy32@gmail.com>
2026-04-23 20:07:12 +0300

    adding neotree

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -5,3 +5,5 @@ vim.g.mapleader = " "
 keymap.set("n", "<leader>w", ":w<CR>")
 
 keymap.set("n", "<leader>q", ":q<CR>")
+
+vim.keymap.set("n", "<leader>e", ":Neotree toggle<CR>", { noremap = true, silent = true })
