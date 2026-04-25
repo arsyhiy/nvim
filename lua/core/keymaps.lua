30040dfae0413063350bc6c445197899a9bd9c03
arsyhiy <arsyhiy32@gmail.com>
2026-04-25 14:44:28 +0300

    adding telescope

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -7,3 +7,10 @@ keymap.set("n", "<leader>w", ":w<CR>")
 keymap.set("n", "<leader>q", ":q<CR>")
 
 vim.keymap.set("n", "<leader>e", ":Neotree filesystem reveal toggle<CR>", { noremap = true, silent = true })
+
+
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
+vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
